c693e6630eb6a61293448cdda30ee0087673dd30
Derrick Hammer <derrick@derrickhammer.com>
2024-03-07 11:42:24 -0500

    fix: disable minify

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     options.banner = {
       js: '"use client"',
     };
+    options.minify = false;
   },
   onSuccess: "tsc --project tsconfig.declarations.json",
 });
